086f7987f92099d2923d1fed962fd1aa09e2605d
Sajib Sarker <sajibsarker1104003@gmail.com>
2021-06-09 00:54:57 +0600

    onprogress function

--- Ajaxbasic/source.js
+++ Ajaxbasic/source.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*document.getElementById('get_data').addEventListener('click', loaddata);
 function loaddata() {
   let xhr = new XMLHttpRequest();
@@ -12,6 +12,7 @@ function loaddata() {
   xhr.send();
   console.log(xhr);
 }*/
+/*
 document.getElementById('get_data').addEventListener('click', loaddata);
 function loaddata() {
   let xhr = new XMLHttpRequest();
@@ -25,8 +26,8 @@ function loaddata() {
   xhr.send();
   console.log(xhr);
 }
-=======
-document.getElementById('get_data').addEventListener('click', loaddata);
+*/
+/*document.getElementById('get_data').addEventListener('click', loaddata);
 function loaddata() {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', 'data.txt', true);
@@ -39,4 +40,21 @@ function loaddata() {
   xhr.send();
   console.log(xhr);
 }
->>>>>>> 4b2fef09a97d6295d6bd5f26bcd59ba5c50a87b9
+>>>>>>> 4b2fef09a97d6295d6bd5f26bcd59ba5c50a87b9*/
+//OnProgress function (it uses after open)
+document.getElementById('get_data').addEventListener('click', loaddata);
+function loaddata() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', 'data.txt', true);
+  xhr.onprogress = function () {
+    console.log(xhr.readyState);
+  }
+  xhr.onload = function () {
+    if (this.status === 200 && this.readyState === 4) {
+      //document.getElementById('output').innerHTML = `<h4>${this.responseText}</h4?`
+      console.log(this.responseText);
+    }
+  }
+  xhr.send();
+  //console.log(xhr);
+}
