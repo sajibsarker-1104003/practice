0f5bd0bf36b9057473911460883b0ecadd3a9dba
Sajib Sarker <sajibsarker1104003@gmail.com>
2021-06-09 00:38:32 +0600

    readystate check

--- Ajaxbasic/source.js
+++ Ajaxbasic/source.js
@@ -1,10 +1,23 @@
-document.getElementById('get_data').addEventListener('click', loaddata);
+/*document.getElementById('get_data').addEventListener('click', loaddata);
 function loaddata() {
   let xhr = new XMLHttpRequest();
   xhr.open('GET', 'data.txt', true);
   xhr.onload = function () {
     if (this.status === 200) {
       document.getElementById('output').innerHTML = `<h4>${this.responseText}</h4?`
+      console.log(this.responseText);
+    }
+  }
+  xhr.send();
+  console.log(xhr);
+}*/
+document.getElementById('get_data').addEventListener('click', loaddata);
+function loaddata() {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', 'data.txt', true);
+  xhr.onreadystatechange = function () {
+    if (this.status === 200 && this.readyState === 4) {
+      //document.getElementById('output').innerHTML = `<h4>${this.responseText}</h4?`
       console.log(this.responseText);
     }
   }
